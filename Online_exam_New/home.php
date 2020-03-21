5566c1e794a8ba9269c2c90c026b0e3a96e6c0f6
BHAIJAN <adesh.chauhan_cs17@gla.ac.in>
2020-03-21 20:16:40 +0530

    CSS of login

---
+++ Online_exam_New/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management system made by Team AAA</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/about-us.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>GLA University is a private recognized university approved by university grants ,[1] located in Mathura Uttar Pradesh, India.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>GLA University was started with the establishment of GLA Institute of Technology & Management offering B.Tech and MCA programs, in the year 1998 by the current Chancellor Mr. Narayan Das Agrawal [6] In the year 1999, MBA program was introduced with 60 seats. It was in 2006 when GLA group of institutions came into existence with establishment of three new institutions, namely GLA Institute of Management, GLA Institute of Professional Studies, and GLA Institute of Pharmaceutical Research. New courses like BBA, BCA, B.Sc. Biotech, B. Pharma were introduced under these institutions. The GLA University, Mathura is managed by GLA Group of Institutions that later, in 2010, grew to become the GLA University through the U.P. State Legislative Act of 2009 (UP Act 21 of 2010). </p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
